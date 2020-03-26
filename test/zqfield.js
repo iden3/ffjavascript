7a39682ab5b11fcba6a04647dca1f9d6bef71de3
Jordi Baylina <jordi@baylina.cat>
2020-03-26 10:59:31 +0100

    Test working

---
+++ test/zqfield.js
@@ -0,0 +1,45 @@
+const ZqField = require("../index.js").ZqField;
+const bigInt = require("big-integer");
+const assert = require("assert");
+
+const q = bigInt("21888242871839275222246405745257275088696311157297823662689037894645226208583");
+const r = bigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+
+describe("F1 testing", () => {
+    it("Should compute euclidean", () => {
+        const F = new ZqField(bigInt(7));
+        const res = F.inv(bigInt(4));
+
+        assert(F.eq(res,bigInt(2)));
+    });
+
+    it("Should multiply and divide in F1", () => {
+        const F = new ZqField(q);
+        const a = bigInt("1");
+        const b = F.normalize(-3);
+        const c = F.mul(a,b);
+        const d = F.div(c,b);
+
+        assert(F.eq(a, d));
+    });
+
+    it("Should compute sqrts", () => {
+        const F = new ZqField(q);
+        const a = bigInt("4");
+        let b = F.sqrt(a);
+        assert(F.eq(bigInt(0), F.sqrt(bigInt("0"))));
+        assert(F.eq(b, bigInt("2")));
+        assert(F.sqrt(F.nqr) === null);
+    });
+
+    it("Should compute sqrt of 100 random numbers", () => {
+        const F = new ZqField(r);
+        for (let j=0;j<100; j++) {
+            let a = F.random();
+            let s = F.sqrt(a);
+            if (s != null) {
+                assert(F.eq(F.square(s), a));
+            }
+        }
+    });
+});
