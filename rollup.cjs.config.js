6b996a203ab78e26d80f4a392130b7a881d0d67c
Jordi Baylina <jordi@baylina.cat>
2020-07-13 07:59:54 +0200

    fix cjs

--- rollup.cjs.config.js
+++ rollup.cjs.config.js
@@ -4,7 +4,7 @@ import commonJS from "rollup-plugin-commonjs";
 export default {
     input: "main.js",
     output: {
-        file: "build/main.js",
+        file: "build/main.cjs",
         format: "cjs",
     },
     external: ["crypto", "os", "worker_threads"],
