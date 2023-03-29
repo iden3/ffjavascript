e1fbfc15d514dde65bb77e4cb3e1e2db8ff1c8ce
Rohin Joshi <rohinjoshi@Rohins-MacBook-Air.local>
2023-03-29 12:45:05 +0530

    base64 add

--- src/threadman.js
+++ src/threadman.js
@@ -25,6 +25,7 @@ const MEM_SIZE = 25;  // Memory size in 64K Pakes (1600Kb)
 import thread from "./threadman_thread.js";
 import os from "os";
 import Worker from "web-worker";
+import {Base64} from 'js-base64';
 
 class Deferred {
     constructor() {
